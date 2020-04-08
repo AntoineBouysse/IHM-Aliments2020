d1bad1266298107a908db2ba7e9a27c628af762f
JulietteDecobert <54988613+JulietteDecobert@users.noreply.github.com>
2020-04-08 17:04:04 +0200

    #1 add map

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,102 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.Drawable;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+  /*  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }*/
+
+
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //load/initialize the osmdroid configuration, this can be done
+        Configuration.getInstance().load(   getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()) );
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
         setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);    //render
+        map.setBuiltInZoomControls(true);               // zoomable
+        //map.setMultiTouchControls(true);                //  zoom with 2 fingers
+
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+        IMapController mapController = map.getController();
+        mapController.setCenter(startPoint);
+        mapController.setZoom(18.0);
+
+
+        //create a new item to draw on the map
+        //your items
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem home = new OverlayItem("J.Decobert", "nos bureaux", new GeoPoint(43.65020,7.00517));
+        Drawable m = home.getMarker(0);
+
+        items.add(home); // Lat/Lon decimal degrees
+        items.add(new OverlayItem("Producteur de lait", "chez ???", new GeoPoint(43.64950,7.00517))); // Lat/Lon decimal degrees
+
+        //the Place icons on the map with a click listener
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(int index, OverlayItem item) {
+                        return false;
+                    }
+                });
+
+
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
